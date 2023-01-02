a06c2b15f56919805e62e29621335bdd23de467f
Oleksii Kutsenko <kutsenko.new@gmail.com>
2023-01-02 14:00:14 +0100

    WIP

---
+++ plans-frontend/src/routes/+layout.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+	import { user } from '$lib/store.ts';
+	import { PUBLIC_API_ROOT } from '$env/static/public'
+
+	let loginData = {
+		email: '',
+		password: ''
+	};
+
+	async function handleLogin(e) {
+		e.preventDefault();
+
+		let response;
+	}
+
+	console.log(PUBLIC_API_ROOT);
+</script>
+
+{#if $user}
+	<nav>
+		<a href="/">Home</a>
+		<a href="/countries">Countries</a>
+	</nav>
+
+	<slot />
+{:else}
+	<label>Email: <input type="email" bind:value={loginData.email} /></label>
+	<label>Password: <input type="password" bind:value={loginData.password} /></label>
+	<button type="submit">Log In</button>
+{/if}
---
+++ plans-frontend/src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+	import { user } from '$lib/store';
+</script>
+
+<div id="root">
+	<h1 class="">Home</h1>
+</div>
